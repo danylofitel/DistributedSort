00acea50f66150c8e08cd4a3c34cdfc7d8738894
Danylo Fitel <danylo.fitel@outlook.com>
2015-08-31 22:52:14 +0100

    Initial commit, working prototype of HeapSort.

---
+++ DistributedSort/DistributedHeapSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Wintellect.PowerCollections;
+
+namespace DistributedSort
+{
+    public static class DistributedHeapSort<T> where T : IComparable<T>
+    {
+        public static IEnumerable<T> Sort(IEnumerable<OrderedBag<T>> remotes)
+        {
+            OrderedBag<Tuple<T, OrderedBag<T>>> buffer =
+                new OrderedBag<Tuple<T, OrderedBag<T>>>((a, b) => a.Item1.CompareTo(b.Item1));
+
+            // Initialize heap.
+            foreach (var remote in remotes)
+            {
+                if (remote.Count > 0)
+                {
+                    T elem = remote.RemoveFirst();
+                    buffer.Add(new Tuple<T, OrderedBag<T>>(elem, remote));
+                }
+            }
+
+            // Retrieve min element step by step.
+            while (buffer.Count > 0)
+            {
+                Tuple<T, OrderedBag<T>> nextMin = buffer.RemoveFirst();
+                yield return nextMin.Item1;
+
+                if (nextMin.Item2.Count > 0)
+                {
+                    buffer.Add(new Tuple<T, OrderedBag<T>>(
+                        nextMin.Item2.RemoveFirst(), nextMin.Item2));
+                }
+            }
+        }
+    }
+}
---
+++ DistributedSort/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Wintellect.PowerCollections;
+
+namespace DistributedSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OrderedBag<int> empty = new OrderedBag<int>(new List<int>
+            { });
+
+            OrderedBag<int> a = new OrderedBag<int>(new List<int>
+                    { 1, 5, 3, 7, 9, 11, 15, 13 });
+
+            OrderedBag<int> b = new OrderedBag<int>(new List<int>
+                    { 10, 4, 6, 2, 8, 12, 14, 0 });
+
+            OrderedBag<int> c = new OrderedBag<int>(new List<int>
+                    { 100, 220, -80, -40, 60});
+
+            OrderedBag<int> d = new OrderedBag<int>(new List<int>
+                    { 121, 64, 81, -144, 16, 25, 49, -36 });
+
+            Random generator = new Random();
+            OrderedBag<int> random = new OrderedBag<int>(
+                Enumerable.Range(0, 20).Select(x => generator.Next() % 1000));
+
+            List<OrderedBag<int>> remotes = new List<OrderedBag<int>> { empty, a, b, c, d, random };
+
+            foreach (var elem in DistributedHeapSort<int>.Sort(remotes))
+            {
+                Console.Write($"{elem}, ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
